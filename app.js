c2e87ead9c6e6d7e4dbf079a1e7ec0517b755649
Marvin Anderson <mlan225@g.uky.edu>
2017-01-26 21:17:54 +0000

    added an or port option

--- app.js
+++ app.js
@@ -238,6 +238,6 @@ function isLoggedIn(req,res,next){
  
  
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 8080, process.env.IP, function(){
     console.log("weatherapi server started!"); 
 });
